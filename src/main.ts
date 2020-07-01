e047e3638b74c26718b57f6443ab97f31ac36e45
Yannic Labonte <labonte@publicplan.de>
2020-07-02 01:03:01 +0200

    Add backend support for aes-192-cbc decryption (#11)

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import {RelayDataInterpreter} from "./lib/relay-data-interpreter";
 import {GetStateCategory, GetStateData} from "./lib/get-state-data";
 import {GetStateDataSysInfo} from "./lib/get-state-data-sys-info";
 import {GetStateDataObject} from "./lib/get-state-data-object";
+import * as crypto from "crypto";
 
 // Augment the adapter.config object with the actual types
 // TODO: delete this in the next version
@@ -55,6 +56,34 @@ export class ProconIp extends utils.Adapter {
     }
 
     private decrypt(value: string, key: string): string {
+        // Fallback to legacy decryption if either value or key are not valid for the AES-192 
+        // variant. See: https://github.com/ioBroker/ioBroker.js-controller/pull/887/files
+        if (!value.startsWith(`$/aes-192-cbc:`) || !/^[0-9a-f]{48}$/.test(key)) {
+            return this.legacyDecrypt(value, key);
+        }
+
+        try {
+            const cipherParts = value.split(":");
+            const cipherSuite = "aes-192-cbc";
+            const cipherKey = Buffer.from(key, "hex");
+            const cipherIv = Buffer.from(cipherParts[1], "hex");
+            const cipherText = Buffer.from(cipherParts[2], "hex");
+            const decipher = crypto.createDecipheriv(cipherSuite, cipherKey, cipherIv);
+            
+            return Buffer.concat([
+                decipher.update(cipherText), 
+                decipher.final()]
+            ).toString();
+        } catch (e) {
+            this.log.warn("Error on decrypting a setting!");
+            this.log.info("Please visit settings and re-enter your connection parameters.");
+            this.log.debug(`Exact error was: ${e}`);
+        }
+
+        return "";
+    }
+
+    private legacyDecrypt(value: string, key: string): string {
         let result = "";
         for (let i = 0; i < value.length; ++i) {
             result += String.fromCharCode(key[i % key.length].charCodeAt(0) ^ value.charCodeAt(i));
