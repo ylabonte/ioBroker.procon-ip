ad59e5e8232a34520db0a77d250482075dd6b190
Yannic Labonte <yannic.labonte@gmail.com>
2025-09-08 05:39:11 +0200

    Fix test setup

--- src/main.test.ts
+++ src/main.test.ts
@@ -8,20 +8,6 @@
 import { expect } from 'chai';
 // import { ProconIp } from './main';
 
-describe('module to test => function to test', () => {
-    // initializing logic
-    const expected = 5;
-
-    it(`should return ${expected}`, () => {
-        const result = 5;
-        // assign result a value from functionToTest
-        expect(result).to.equal(expected);
-        // or using the should() syntax
-        result.should.equal(expected);
-    });
-    // ... more tests => it
-});
-
 describe('Controller URL pre-check', () => {
     it(`URL('') should throw TypeError [ERR_INVALID_URL]`, () => {
         expect(() => new URL('')).to.throw('Invalid URL');
